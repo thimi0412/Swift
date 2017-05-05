b22562e5d955de7d57348350eb74013fe066b1c5
KosukeShimizu <thimi0412@icloud.com>
2017-05-05 21:20:20 +0900

    first commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTwitter
+//
+//  Created by 清水幸佑 on 2017/05/05.
+//  Copyright © 2017年 清水幸佑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
